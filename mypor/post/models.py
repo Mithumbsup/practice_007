65fd7bb5f40b870e30be78c453ca057bb07d2d70
mi <sosgik96@naver.com>
2019-07-24 15:48:50 +0900

    first

--- mypor/post/models.py
+++ mypor/post/models.py
@@ -4,6 +4,13 @@ from django.db import models
 from multiselectfield import MultiSelectField
 
 # Create your models here.
+city_location = (
+            ('S','Study_Room'), 
+            ('P','Practice_Room'),
+            ('C','Concert_Room'),
+            ('E','etc'),
+            ('?','etc_what'),
+            )
 
 
 def default_city(): # user가 회원가입할 때 지정한? 도시 or seoul
@@ -16,15 +23,6 @@ class Post(models.Model):
         ('practice','PracticeRoom'),
         ('etc','etc'),
         )
-    
-    city_location = (
-            ('S','Study_Room'), 
-            ('P','Practice_Room'),
-            ('C','Concert_Room'),
-            ('E','etc'),
-            ('?','etc_what'),
-            )
-
         
     title = models.CharField(max_length=50)
     context = models.TextField()
@@ -32,10 +30,6 @@ class Post(models.Model):
     location = models.CharField(choices=city_location , max_length=30, default=default_city)
     category = MultiSelectField(choices=ITEM_category, max_length=50,default = 'study', blank=True)
     etc_what = models.CharField(max_length=50, null=True,blank=True)
-
-
-
-
     created_date = models.DateTimeField(auto_now_add=True)  # 글 작성 시간 :  시간이 있는 날짜를 저장하는 datetime 필드를 만들기 
     Modified_date = models.DateTimeField(auto_now=True) # 글 게시 날짜 
 
